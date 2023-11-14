30a4536b1b2142c92883fc85e50c899f40d67e11
yrvane <118963840+yrvane@users.noreply.github.com>
2023-11-14 19:41:15 +0100

    Update list.c
    liste. c mise a jour

--- list.c
+++ list.c
@@ -2,28 +2,63 @@
 // Created by Yrvan on 12/11/2023.
 //
 #include "list.h"
-#include "stdlib.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-t_cell*createCell(int val){
-    t_cell*l =(int*)malloc(sizeof(t_cell));
-    l->value= val;
-    l->next=NULL;
-    return l;
+t_cell* createCell(int val, int levels){
+    t_cell* p =(t_cell*)malloc(sizeof(t_cell));
+    p->value= val;
+    p->levels = levels;
+    p->next = (t_cell**)malloc(levels*sizeof(t_cell*));
+    for(int i =0; i<levels; i++){
+        p->next[i] = NULL;
+    }
+    return p;
 }
 
-t_cell*createEmptyList(){
-    t_cell*l=(int*)malloc(sizeof(t_cell));
-    l=NULL;
+
+
+level_list CreateEmptyList(int max_levels){
+    level_list l;
+    l.max_level = max_levels;
+    l.heads = (t_cell**)malloc(max_levels*sizeof(t_cell*));
+    for(int i =0; i<max_levels; i++){
+        l.heads[i]= NULL;
+    }
     return l;
+
 }
 
-struct level_list*createlevel_list(int val, int c, t_cell*h){
-    struct level_list*l=(int*)malloc(sizeof(struct level_list));
-    l->max_level=val;
-    l->h->value=c;
-    l->h->next=NULL;
-    return l;
+level_list InsertCell( int level, t_cell*p, level_list * l ){
+    while(level>0) {
+        l->heads[level] = p->next[level];
+        return InsertCell(level, p, l->heads[level-1]);
+    }
 
+}
+
+void print_LevelList(level_list*l, int level, t_cell*p){
+    if ((level>l->max_level)|| (level<0)){
+        printf("le niveau n'existe pas");
+    }
+    else{
+        while (l->heads[level]!=0){
+            printf("%d", l->heads[level]);
+            l->heads[level]=l->heads[level]->next;
+        }
+    }
+}
+
+void print_AllLevelList(level_list*l){
+    if (l->max_level=0) {
+        printf("la liste est vide");
+    }
+    for(int i=0;i<l->max_level;i++){
+        while(l->heads[i]!=NULL){
+            printf("%d", l->heads[i]);
+            l->heads[i]=l->heads[i]->next;
+        }
+    }
 }
 
 
