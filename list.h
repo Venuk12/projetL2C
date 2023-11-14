5b7e1456e503b6852c2ef49e51868b6330ce7dcc
yrvane <118963840+yrvane@users.noreply.github.com>
2023-11-14 19:40:11 +0100

    Update list.h
    liste mise a jour

--- list.h
+++ list.h
@@ -1,36 +1,30 @@
 //
 // Created by Yrvan on 12/11/2023.
 //
-
 #ifndef PORJET_C__LIST_H
 #define PORJET_C__LIST_H
 
-#endif //PORJET_C__LIST_H
+
 
 typedef struct s_cell{
     int value;
-    struct t_cell* next;
+    int levels;
+    struct s_cell** next;
 }t_cell;
 
-typedef struct s_ht_list{
-    t_cell*head;
-    t_cell*tail;
-}t_ht_list;
 
 typedef struct level_list{
     int max_level;
-    struct t_cell* h;
-};
+    t_cell** heads;
+}level_list;
 
-typedef struct s_d_list{
-    int value;
-    int level;
-   struct level_list** tab_head;
-}t_d_cell;
+t_cell* createCell(int, int);
+level_list createEmptyList();
+level_list InsertCell(int level, t_cell*p, level_list * l);
+void print_LevelCell(level_list*l, int level);
+void print_AllLevelList(level_list*l);
 
-t_cell*createCell(int);
-t_cell*createEmptyList();
-struct level_list*createlevel_list(int, int, t_cell*);
+#endif //PORJET_C__LIST_H
 
 
 
