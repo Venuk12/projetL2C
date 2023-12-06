0aa38c24dc09bf8589fd232a5e4e104f4b51338f
yrvane <118963840+yrvane@users.noreply.github.com>
2023-12-06 12:22:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,15 @@
 #include "list.c"
 
 int main() {
-    int val1=6, first_niv=3;
-    struct level_list*l1= createCell(val1);
+    //printf("hello word");
+    //level_list* mylist= createEmptyList(7); //création d'une liste a niveau
+    //insererTriee(mylist, createCell(6,2)); //insertion  cellule à niveaux
+    //insererTriee(mylist, createCell(18,6));
+    //print_AllLevelList(mylist); // affichage de tous les niveau de la liste
+    int n=4;
+    level_list*l2=createcomplex_list(n);
+    search_value(n,l2);
+    return 0;
 
 
 }
